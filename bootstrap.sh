3fa4c66b8643356a7cf611e5582e771742292949
Gian Inductivo <gian@dynamicobjx.com>
2015-04-07 11:18:24 +0800

    edits. topic done

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,25 @@
 
 set -x
 
+SCALA_VER=2.11.2
+ACTIVATOR_VER=1.3.2
+MYSQL_ROOT_PW="cookbook"
+
 if [ ! -e "/home/vagrant/.firstboot" ]; then
-  # Docker
+  
+  # tools etc
+  yum -y install yum-plugin-fastestmirror
+  yum -y install git wget curl rpm-build
+  
+  # Pre-docker install: http://stackoverflow.com/a/27216873
+  sudo yum-config-manager --enable public_ol6_latest
+  sudo yum install -y device-mapper-event-libs
+
+  # Docker 
   yum -y install docker-io
   service docker start
   chkconfig docker on
-
-  # Install MySQL
-  yum -y install mysql
+  usermod -a -G docker vagrant
 
   # Install the JDK
   curl -LO 'http://download.oracle.com/otn-pub/java/jdk/7u51-b13/jdk-7u51-linux-x64.rpm' -H 'Cookie: oraclelicense=accept-securebackup-cookie'
@@ -17,43 +28,26 @@ if [ ! -e "/home/vagrant/.firstboot" ]; then
   /usr/sbin/alternatives --install /usr/bin/java java /usr/java/default/bin/java 200000
   rm jdk-7u51-linux-x64.rpm
 
-  # Install scala
-  rpm -ivh http://scala-lang.org/files/archive/scala-2.10.4.rpm
+  # Install scala repl
+  rpm -ivh http://scala-lang.org/files/archive/scala-$SCALA_VER.rpm
   
-  # Install sbt
-  su - vagrant -c "mkdir /home/vagrant/bin"
-  curl -s https://raw.github.com/paulp/sbt-extras/master/sbt > /home/vagrant/bin/sbt && chmod 0755 /home/vagrant/bin/sbt 
-  su - vagrant -c "/home/vagrant/bin/sbt -sbt-create"
-
   # Install Activator
   cd /opt
-  sudo wget http://downloads.typesafe.com/typesafe-activator/1.1.1/typesafe-activator-1.1.1-minimal.zip
-  sudo unzip typesafe-activator-1.1.1-minimal.zip 
-  sudo chown vagrant:vagrant /opt/activator-1.1.1-minimal
-
-  # tools etc
-  yum -y install git wget zsh fakeroot dpkg
-  su - vagrant -c "git config --global user.name "Giancarlo Inductivo""
-  su - vagrant -c "git config --global user.email "gian@dynamicobjx.com""
-
-  # Install Oh-My-Zsh
-  su - vagrant -c "curl -L http://install.ohmyz.sh | sh"
-  su - vagrant -c "chsh -s /bin/zsh"
-
-  # Install giter8
-  su - vagrant -c "curl https://raw.githubusercontent.com/n8han/conscript/master/setup.sh | sh"
-  su - vagrant -c "cs n8han/giter8"
+  wget http://downloads.typesafe.com/typesafe-activator/$ACTIVATOR_VER/typesafe-activator-$ACTIVATOR_VER.zip
+  unzip typesafe-activator-$ACTIVATOR_VER.zip 
+  chown -R vagrant:vagrant /opt/activator-$ACTIVATOR_VER
 
   # Local time
   mv /etc/localtime /etc/localtime.bak
   ln -s /usr/share/zoneinfo/Asia/Manila /etc/localtime
 
+  # Set Path
+  echo "export JAVA_HOME=/usr/java/default/" >> /home/vagrant/.bash_profile
+  echo "export PATH=$PATH:$JAVA_HOME/bin:/home/vagrant/bin:/opt/activator-$ACTIVATOR_VER" >> /home/vagrant/.bash_profile
+  
   touch /home/vagrant/.firstboot
 fi
 
 # Start MySQL
-docker run -e MYSQL_PASS="Proverbs1423" -i -t -p 3306:3306 --name mysqld -d tutum/mysql
-
-export JAVA_HOME=/usr/java/default/
-export PATH=$PATH:$JAVA_HOME/bin:/home/vagrant/bin:/opt/activator-1.1.1-minimal
+docker run -e MYSQL_PASS=$MYSQL_ROOT_PW -i -d -p 3306:3306 --name mysqld -t tutum/mysql
 
